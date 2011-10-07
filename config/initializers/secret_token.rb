861ad5d1c9e9de511b322ab092955a3c638c4f59
Gustavo Roberto <gustavoroberto77@gmail.com>
2011-10-07 10:41:17 -0300

    Iniciando rails do zero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagamento::Application.config.secret_token = '9cbce714adc3e5e0239838fcec841ca5bd79f3158fb8f182dfa10666a66d9b40b425848374903950ce2b6fe5ec7bcea37478c5179651d609f7dd97991d811fa4'
