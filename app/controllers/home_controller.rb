ef206fc879dbc0155bea210adbbbcc90b1e0d079
Gustavo Roberto <gustavoroberto77@gmail.com>
2011-10-14 11:47:56 -0300

    Fornecedores é a homepage a partir de agora

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-	before_filter :authenticate_usuario!
-	
-  def index
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Pagamento::Application.routes.draw do
 
   devise_for :usuarios
 
-  root :to => 'home#index'
+  root :to => 'fornecedores#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
