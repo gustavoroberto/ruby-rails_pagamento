3866ad801fdbd276680a54ed18736a9ddd517644
Gustavo Roberto <gustavoroberto77@gmail.com>
2011-10-14 11:35:41 -0300

    Rerirado view show, protecao de fornecedores.

--- app/controllers/fornecedores_controller.rb
+++ app/controllers/fornecedores_controller.rb
@@ -1,35 +1,32 @@
+# encoding: utf-8
 class FornecedoresController < ApplicationController
+  before_filter :authenticate_usuario!, except: [:index]
+
   # GET /fornecedores
-  # GET /fornecedores.json
   def index
     @fornecedores = Fornecedor.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @fornecedores }
-    end
   end
 
-  # GET /fornecedores/1
-  # GET /fornecedores/1.json
-  def show
-    @fornecedor = Fornecedor.find(params[:id])
+  # # GET /fornecedores/1
+  # # GET /fornecedores/1.json
+  # def show
+  #   @fornecedor = Fornecedor.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @fornecedor }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @fornecedor }
+  #   end
+  # end
 
   # GET /fornecedores/new
   # GET /fornecedores/new.json
   def new
     @fornecedor = Fornecedor.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @fornecedor }
-    end
+    # respond_to do |format|
+    #   format.html # new.html.erb
+    #   format.json { render json: @fornecedor }
+    # end
   end
 
   # GET /fornecedores/1/edit
@@ -38,46 +35,33 @@ class FornecedoresController < ApplicationController
   end
 
   # POST /fornecedores
-  # POST /fornecedores.json
   def create
     @fornecedor = Fornecedor.new(params[:fornecedor])
 
-    respond_to do |format|
       if @fornecedor.save
-        format.html { redirect_to @fornecedor, notice: 'Fornecedor was successfully created.' }
-        format.json { render json: @fornecedor, status: :created, location: @fornecedor }
+        redirect_to fornecedores_path, notice: 'Fornecedor cadastrado com sucesso.'
       else
-        format.html { render action: "new" }
-        format.json { render json: @fornecedor.errors, status: :unprocessable_entity }
+        render action: "new"
       end
-    end
+
   end
 
   # PUT /fornecedores/1
-  # PUT /fornecedores/1.json
   def update
     @fornecedor = Fornecedor.find(params[:id])
 
-    respond_to do |format|
       if @fornecedor.update_attributes(params[:fornecedor])
-        format.html { redirect_to @fornecedor, notice: 'Fornecedor was successfully updated.' }
-        format.json { head :ok }
+        redirect_to fornecedores_path, notice: 'Fornecedor atualizado com sucesso.' 
       else
-        format.html { render action: "edit" }
-        format.json { render json: @fornecedor.errors, status: :unprocessable_entity }
+        render action: "edit"
       end
-    end
   end
 
   # DELETE /fornecedores/1
-  # DELETE /fornecedores/1.json
   def destroy
     @fornecedor = Fornecedor.find(params[:id])
     @fornecedor.destroy
 
-    respond_to do |format|
-      format.html { redirect_to fornecedores_url }
-      format.json { head :ok }
-    end
+      redirect_to fornecedores_url, notice: 'Fornecedor excluído.'
   end
 end
