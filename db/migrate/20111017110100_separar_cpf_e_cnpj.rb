261db17aa1ab0c7989d9f2c1a1ec8ab2100814c2
Gustavo Roberto <gustavoroberto77@gmail.com>
2011-10-17 08:12:51 -0300

    Separamos cnpj e cpf no banco

---
+++ db/migrate/20111017110100_separar_cpf_e_cnpj.rb
@@ -0,0 +1,7 @@
+class SepararCpfECnpj < ActiveRecord::Migration
+  def change
+  	rename_column :fornecedores, :cnpj_cpf, :cnpj
+  	add_column :fornecedores, :cpf, :string
+  	add_column :fornecedores, :pj, :string
+  end
+end
