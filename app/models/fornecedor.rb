ed0f1567f34d1066e1b63f984acc05d09b2c911f
Gustavo Roberto <gustavoroberto77@gmail.com>
2011-10-17 11:12:10 -0300

    validação de campos

--- app/models/fornecedor.rb
+++ app/models/fornecedor.rb
@@ -1,7 +1,21 @@
+# encoding: utf-8
+
 require "brcpfcnpj"
 
 class Fornecedor < ActiveRecord::Base
 	validates :nome, presence: true
 	usar_como_cpf :cpf
 	usar_como_cnpj :cnpj
+	validate :cpf_ou_cnpj
+
+private
+	def cpf_ou_cnpj
+		if cpf.present? and cnpj.present?
+			errors[:base] << "Use CNPJ ou CPF"
+		end
+
+		if cpf.blank? and cnpj.blank?
+			errors[:base] << "Use ao menos CNPJ ou CPF"
+		end
+	end
 end
--- spec/models/fornecedor_spec.rb
+++ spec/models/fornecedor_spec.rb
@@ -3,18 +3,39 @@
 require 'spec_helper'
 
 describe Fornecedor do
+	
 	it "válido com nome" do
-		Fornecedor.new(nome: 'Apple').should be_valid
+		Fornecedor.new(nome:'Apple', cnpj:'00000000000191').should be_valid
 	end
+	
 	it "não é válido sem nome" do
-		Fornecedor.new.should be_invalid
+		Fornecedor.new(cnpj:'00000000000191').should be_invalid
 	end
+	
 	it "valida CPF" do
 		Fornecedor.new(nome:'Gustavo', cpf:'80785433104').should be_valid
 		Fornecedor.new(nome:'Fulano', cpf:'1').should be_invalid
 	end
+	
 	it "valida CNPJ" do
 		Fornecedor.new(nome:'Apple', cnpj:'00000000000191').should be_valid
 		Fornecedor.new(nome:'MS', cnpj:'1').should be_invalid
 	end
+	
+	it "não aceita CPF e CNPJ juntos" do
+		Fornecedor.new(nome:'Apple', cnpj:'00000000000191', cpf:'80785433104').should be_invalid
+	end
+	
+	it "não aceita CPF e CNPJ vazios ao mesmo tempo" do
+		Fornecedor.new(nome: 'Apple').should be_invalid
+	end
+	
+	it "obriga cnpj se é pj" do
+		Fornecedor.new(nome:'Apple', pj: true, cnpj:'00000000000191').should be_valid
+		Fornecedor.new(nome:'Apple', pj: true, cpf:'80785433104').should be_invalid
+	end
+	
+	it "obriga cpf se não é pj" do
+	end
+	
 end
